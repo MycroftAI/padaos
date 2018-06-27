64b175fe292d90ab0651f8185897d20b604bf29c
Åke Forslund <ake.forslund@gmail.com>
2018-06-27 14:16:44 +0200

    replace builtin re with regex module
    python <= 3.4 (and perhaps early versions of python 3.5) has a limit of number of named groups set to 100. See https://bugs.python.org/issue22437
    
    In mycroft the number of groups quickly surpasses 100 and training won't complete. This is a quick drop in replacement using the re compatible module regex.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from os.path import abspath, join, dirname
+
+with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
+    requirements = f.readlines()
 
 setup(
     name='padaos',
-    version='0.1.4',
+    version='0.1.5',
     description='A rigid, lightweight, dead-simple intent parser',
     url='http://github.com/MatthewScholefield/padaos',
     author='Matthew Scholefield',
     author_email='matthew331199@gmail.com',
     license='MIT',
+    install_requires=requirements,
     py_modules=[
         'padaos'
     ],
