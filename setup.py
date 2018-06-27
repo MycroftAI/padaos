17ec299ffa52cfefab90e03463c308783fd8ce95
Matthew Scholefield <matthew331199@gmail.com>
2018-06-27 11:52:36 -0500

    Increment version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='padaos',
-    version='0.1.4',
+    version='0.1.5',
     description='A rigid, lightweight, dead-simple intent parser',
     url='http://github.com/MatthewScholefield/padaos',
     author='Matthew Scholefield',
